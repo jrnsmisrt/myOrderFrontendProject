d37f41172787de05670a36c34f2de6c9ff55f49b
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-12 11:16:54 +0100

    adding placeholder for items

--- src/app/item-gallery/item-gallery.component.ts
+++ src/app/item-gallery/item-gallery.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ItemService} from "../service/item.service";
+import {Item} from "../model/Item";
 @Component({
   selector: 'app-item-gallery',
   templateUrl: './item-gallery.component.html',
   styleUrls: ['./item-gallery.component.css']
 })
 export class ItemGalleryComponent implements OnInit {
+  items: Item[] = [];
 
-  constructor() { }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
+    this.getItems();
+  }
+  getItems(){
+    this.itemService.getItems().subscribe(items => this.items = items);
   }
 
 }
