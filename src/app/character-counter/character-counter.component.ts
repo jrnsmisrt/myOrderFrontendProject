968158aed7dc55435c499dbdaf5dac5ff6853a6e
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-13 11:53:40 +0100

    finished story 3&4

--- src/app/character-counter/character-counter.component.ts
+++ src/app/character-counter/character-counter.component.ts
@@ -3,38 +3,38 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-character-counter',
   templateUrl: './character-counter.component.html',
-  styleUrls: ['./character-counter.component.css']
+  styleUrls: ['./character-counter.component.css'],
+
 })
 export class CharacterCounterComponent implements OnInit {
-  maxLengthDescription: number;
+  maxLengthCharacters: number;
   counter: number;
 
+
   constructor() {
-    this.maxLengthDescription = 255;
-    this.counter = this.maxLengthDescription;
+    this.maxLengthCharacters = 255;
+    this.counter = this.maxLengthCharacters;
+
   }
 
   ngOnInit(): void {
   }
 
   setmaxLengthDescription(maxLength: number) {
-    this.maxLengthDescription = maxLength;
+    this.maxLengthCharacters = maxLength;
   }
 
   countCharactersLeft(input: string): number {
-
     if (input != null) {
-
-      if (this.counter>0) {
-        this.counter = (this.maxLengthDescription - input.length);
-        return this.counter;
-
-      } else {
-        return 0
+      if (input.length < this.maxLengthCharacters) {
+        if (this.counter > 0) {
+          this.counter = (this.maxLengthCharacters - input.length);
+          return this.counter;
+        }
       }
-    } else {
-      return this.maxLengthDescription;
+      return 0
     }
+    return this.maxLengthCharacters;
 
   }
 }
--- src/app/create-item/create-item.component.ts
+++ src/app/create-item/create-item.component.ts
@@ -13,15 +13,29 @@ import {CharacterCounterComponent} from "../character-counter/character-counter.
 export class CreateItemComponent implements OnInit {
   createItemForm = this.formBuilder.group({
 
-    name: '',
-    description: new FormControl('',[
+    name: new FormControl('',[
       Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+    ]),
+    description: new FormControl('',[
+
       Validators.maxLength(255),
     ]),
-    price: '',
-    amountOfStock: '',
+    price: new FormControl('',[
+      Validators.required,
+      Validators.min(0),
+      ]),
+    amountOfStock: new FormControl('',[
+      Validators.required,
+      Validators.min(0),
+    ])
 
   });
+
+  counterColor: string = 'greenyellow';
+
+  validatorErrorMessage: string;
 
 
   constructor(private itemService: ItemService,
@@ -29,6 +43,7 @@ export class CreateItemComponent implements OnInit {
               private route: ActivatedRoute,
               private router: Router,
               public characterCounter: CharacterCounterComponent) {
+    this.validatorErrorMessage = '';
 
   }
 
@@ -51,6 +66,33 @@ export class CreateItemComponent implements OnInit {
 
   countCharacters(): number {
     let input = this.createItemForm.get('description')?.value;
+    if(input.length===0) this.counterColor="red";
     return this.characterCounter.countCharactersLeft(input);
+  }
+
+  countColor():boolean{
+    if(this.countCharacters()>0){
+      return true;
+    }
+    return false;
+  }
+
+  getFormAttribute(attribute: string): any{
+    return this.createItemForm.get(`${attribute}`);
+  }
+
+  getErrorMessageForValidation(formAttribute: string): string{
+    let attribute=this.getFormAttribute(formAttribute);
+    let error = attribute.errors.error;
+
+
+    switch(error){
+      case 'maxLength': this.validatorErrorMessage = `You have exceeded the maximum allowed characters (${this.characterCounter.maxLengthCharacters})`; break;
+      case 'required': this.validatorErrorMessage = `input field is required`; break;
+      case 'min' : this.validatorErrorMessage = 'Value must be bigger than or equal to 0'; break;
+
+    }
+
+    return this.validatorErrorMessage;
   }
 }
