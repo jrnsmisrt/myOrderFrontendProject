6e55fb69936786e5bc32e6064887aa306c7c23d3
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-14 16:19:05 +0100

    first/lastname validation implemented in html

--- src/app/customer/create-customer/create-customer.component.ts
+++ src/app/customer/create-customer/create-customer.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CustomerService} from "../../service/customer.service";
-import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute} from "@angular/router";
 import {Router} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-create-customer',
@@ -10,28 +12,70 @@ import {Router} from "@angular/router";
   styleUrls: ['./create-customer.component.css']
 })
 export class CreateCustomerComponent implements OnInit {
-  mailLocalPart:string='';
-  mailDomain:string='';
-
-  createCustomerForm= this.formBuilder.group({
-    firstname: '',
-    lastname:'',
-    mail:'',
-    address:'',
-    phoneNumber:'',
+  complete:string='';
+  createCustomerForm = this.formBuilder.group({
+    firstname: new FormControl('',[
+    Validators.required,
+      Validators.minLength(2)
+    ]),
+    lastname: new FormControl('',[
+      Validators.required,
+      Validators.minLength(2)
+    ]),
+    email: new FormGroup({
+      localPart:new FormControl('',[
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      domain: new FormControl('',[
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      complete: new FormControl('',[
+        Validators.required,
+        Validators.min(2),
+        Validators.email
+      ]),
+    }),
+    address: new FormGroup( {
+      streetName: new FormControl('',[
+        Validators.required,
+        Validators.min(2)
+      ]),
+      houseNumber: new FormControl('',[
+        Validators.required,
+        Validators.min(2)
+      ]),
+      postalCode: new FormControl('',[
+        Validators.required,
+        Validators.min(0),
+        Validators.max(9999)
+      ]),
+      country:new FormControl('',[
+        Validators.required
+      ]),
+    }),
+    phoneNumber: new FormGroup({
+      countryCallingCode: new FormControl(''),
+      number: new FormControl(''),
+    })
   })
 
-  constructor(private customerService:CustomerService,
-              private route:ActivatedRoute,
+  constructor(private customerService: CustomerService,
+              private route: ActivatedRoute,
               private router: Router,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              ) {
 
   }
 
   ngOnInit(): void {
+
   }
 
   onSubmit(): void {
+    this.complete=`${this.createCustomerForm.get('email.localPart')?.value}@${this.createCustomerForm.get('email.domain')?.value}`;
+    this.createCustomerForm.get('email.complete')?.patchValue(this.complete);
     this.customerService.createCustomer(this.createCustomerForm.value)
       .subscribe(customer => {
           this.createCustomerForm.reset();
@@ -40,4 +84,7 @@ export class CreateCustomerComponent implements OnInit {
       );
   }
 
+  getFormAttribute(attribute: string): any{
+    return this.createCustomerForm.get(`${attribute}`);
+  }
 }
